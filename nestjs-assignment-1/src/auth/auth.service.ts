3f81486edb3a92dcf974a932ca92b8e9c58690f7
ankur-solutelabs <ankur.p@solutelabs.com>
2021-11-16 10:54:43 +0530

    auth module added and defined + password hashed by  bcrypt

--- nestjs-assignment-1/src/auth/auth.service.ts
+++ nestjs-assignment-1/src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialDto } from './dto/auth-crendentials.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository,
+    ) {}
+
+    async signUP(authCredentialDto:AuthCredentialDto): Promise<void>{
+        return this.userRepository.signUp(authCredentialDto);
+    }
+
+
+}
--- nestjs-assignment-1/src/auth/user.repository.ts
+++ nestjs-assignment-1/src/auth/user.repository.ts
@@ -1,8 +1,40 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import {EntityRepository,Repository} from "typeorm";
+import { AuthCredentialDto } from "./dto/auth-crendentials.dto";
 import {User} from "./user.entity"
+import * as bcrypt from 'bcrypt';
+ 
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+    async signUp(authCredentialDto: AuthCredentialDto):Promise<void> {
+        const {username,password} = authCredentialDto;
 
-    //logic here
+        // const salt = await bcrypt.genSalt();
+        // console.log(salt)
+        
+        const user = new User();
+        user.username = username;
+        user.salt = await bcrypt.genSalt();;
+        user.password = await this.hashedPassword(password,user.salt);
+
+        try { 
+
+            await user.save();
+            
+        } catch (error) {
+           // console.log(error.code)
+           if(error.code === '23505') {
+               throw new ConflictException('Username Already Exists');
+           } else {
+               throw new InternalServerErrorException();
+           }
+            
+        }
+    }
+
+    private async hashedPassword(password: string, salt: string):Promise<string> {
+        return bcrypt.hash(password,salt)
+    }
+  
 }
