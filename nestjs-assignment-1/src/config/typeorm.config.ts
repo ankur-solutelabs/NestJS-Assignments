f40e11c0d16c2622a9110e95e38ab8fc70810500
ankur-solutelabs <ankur.p@solutelabs.com>
2021-11-29 12:06:55 +0530

    database details updated

--- nestjs-assignment-1/src/config/typeorm.config.ts
+++ nestjs-assignment-1/src/config/typeorm.config.ts
@@ -3,9 +3,9 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 export const typeOrmConfig : TypeOrmModuleOptions = {
     type: 'postgres',
     host: 'localhost',
-    port: 3000,
+    port: 5432,
     username: 'postgres',
-    password: 'ankur.p@solutelabs.com',
+    password: 'ankur@123',
     database: 'adminTeacherMgmt',
     entities: [__dirname+ '/../**/*.entity.js'],
     synchronize: true,
